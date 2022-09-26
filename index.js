30b53b6cda818667cd99572f5b0829dc19be4641
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-09-26 16:20:16 +0100

    Chore: Updated search parameter

--- index.js
+++ index.js
@@ -28,17 +28,17 @@ app.use(cors());
 app.post('/waitlist', async (req,res) => {
   const email = req.body.email;
   const time = new Date();
-  if (!db.waitlist.findOne({ email })){
-    db.collection('waitlist')
-      .insertOne({email, time})
-      .then((result) => {
-        sendMail(req,res)
-      })
-      .catch(err =>  {return res.status(500).json({error: "Could not create a new document"})})
-  }
-  else {
-    return res.status(205);
-  }
+  // if (!db.waitlist.findOne({ email })){
+  // }
+  // else {
+  //   return res.status(205);
+  // }
+  db.collection('waitlist')
+    .insertOne({email, time})
+    .then((result) => {
+      sendMail(req,res)
+    })
+    .catch(err =>  {return res.status(500).json({error: "Could not create a new document"})})
 })
 
 async function sendMail(req,res) {
