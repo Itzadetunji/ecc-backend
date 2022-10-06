b9923f6389feccfc70506a888be1e61b3eb425b0
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-10-06 20:24:33 +0100

    Chore: Updated mongodb uri

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ let dbConnection;
 module.exports = {
 	connectToDb: (cb) => {
 		MongoClient.connect(
-			"mongodb+srv://itzadetunji:adetunjimay29@ecc.nnqlkbz.mongodb.net/?retryWrites=true&w=majority"
+			"mongodb+srv://itzadetunji:adetunjimay29@ecc.nnqlkbz.mongodb.net/waitlist?retryWrites=true&w=majority"
 		)
 			.then((client) => {
 				dbConnection = client.db();
